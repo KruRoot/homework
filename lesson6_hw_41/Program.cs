6d15c9673d0ec5cdfb4c0001091787e8f944df69
Andrei <andreyyuryev@yahoo.com>
2022-08-30 01:18:15 +0400

    add hw 41

---
+++ lesson6_hw_41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Homework for lesson 6, #41
+
+int FindPositiveNumbers(int[] arrayForSearch)
+{
+    int positiveNumbers = 0;
+    for (int i = 0; i < arrayForSearch.Length; i++)
+    {
+        if (arrayForSearch[i] > 0)
+        {
+            positiveNumbers++;
+        }
+    }
+    return positiveNumbers;
+}
+
+Console.WriteLine("Enter the numbers (separated by space): ");
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int positiveNumbers = FindPositiveNumbers(numbers);
+Console.WriteLine($"Positive numbers: {positiveNumbers}");
